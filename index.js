89d679dc1aa207e1600c93b27625ec89dc67892d
James Addison <71357672+siliconjungle@users.noreply.github.com>
2022-11-20 23:10:59 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const shouldSet = ([seq, agentId], [seq2, agentId2]) =>
+  seq2 > seq || (seq === seq2 && agentId > agentId2)
+
+class Store {
+  documents = {}
+  versions = {}
+  latestSeq = -1
+
+  applyOps (ops) {
+    // ops: [
+    //   {
+    //     version: [seq, agentId],
+    //     id: ‘test’,
+    //     fields: [0, 1, 2],
+    //     values: [5, 10, ‘hello’],
+    //   },
+    // ]
+
+    const filteredOps = []
+
+    for (const op of ops) {
+      const { version, id, fields, values } = op
+      this.versions[id] ??= []
+      this.documents[id] ??= []
+
+      for (let i = 0; i < fields.length; i++) {
+        if (shouldSet(this.versions[id][fields[i]], version)) {
+          this.versions[id][fields[i]] = version
+          this.documents[id][fields[i]] = values[i]
+
+          filteredOps.push(op)
+        }
+      }
+
+      this.latestSeq = Math.max(this.latestSeq, version[0])
+    }
+
+    return filteredOps
+  }
+}
+
+export default Store
